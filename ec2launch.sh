52ba9f2772b28bb47ea8e2348dc1a307f4f48d8f
Siny <sinydaniel93@gmail.com>
2022-02-24 20:30:07 +0530

    template modified

--- ec2launch.sh
+++ ec2launch.sh
@@ -2,4 +2,4 @@
 
 TEMP_ID="lt-091dc1f3bbce4364e"
 TEMP_VER=2
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} | jq
