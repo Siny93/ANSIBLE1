674ac330c1072fde34a2e17761b41de7e64781e2
Siny <sinydaniel93@gmail.com>
2022-02-24 20:25:51 +0530

    template modified

--- ec2launch.sh
+++ ec2launch.sh
@@ -2,5 +2,4 @@
 
 TEMP_ID="lt-091dc1f3bbce4364e"
 TEMP_VER=2
-
-ec2 run instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}
+aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER}
