9f8ee0771c69b43d415d7376ee4f54cf21188881
Siny <sinydaniel93@gmail.com>
2022-02-25 21:53:18 +0530

    record file

--- ec2launch.sh
+++ ec2launch.sh
@@ -11,18 +11,30 @@ TEMP_VER=2
 ZONE_ID=Z0763712QSDNO6WPG06S
 
 # check if instance is already there
-aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
+CREATE_INSTANCE() {
+  aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;33minstance is already there\e[0m"
+  if [ $? -eq 0 ]; then
+    echo -e "\e[1;33minstance is already there\e[0m"
+  else
+      aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  fi
+  sleep 10
+
+
+  IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+
+  # update a dns record
+  sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+  aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
+
+}
+
+if [ "$COMPONENT" == "all" ]; then
+  for comp in frontend mongodb catalogue ; do
+    COMPONENT=$comp
+    CREATE_INSTANCE
+  done
 else
-    aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]" "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+  CREATE_INSTANCE
 fi
-
-
-
-IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
-
-# update a dns record
-sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
-aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
