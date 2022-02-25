3f6ab637fbd0de260db0289c1192eb37727d4875
Siny <sinydaniel93@gmail.com>
2022-02-25 20:40:00 +0530

    template modified

--- ec2launch.sh
+++ ec2launch.sh
@@ -8,16 +8,22 @@ COMPONENT=$1
 
 TEMP_ID="lt-091dc1f3bbce4364e"
 TEMP_VER=2
+ZONE_ID=Z0763712QSD
 
 # check if instance is already there
 aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].State.Name | sed 's/"//g' | grep -E 'running|stopped' &>/dev/null
 
 if [ $? -eq 0 ]; then
   echo -e "\e[1;33minstance is already there\e[0m"
-  exit
+else
+    aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"
+    "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 fi
 
 
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${TEMP_ID},Version=${TEMP_VER} --tag-specifications "ResourceType=spot-instances-request,Tags=[{Key=Name,Value=${COMPONENT}}]"
-"ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+IPADDRESS=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${COMPONENT}" | jq .Reservations[].Instances[].PrivateIpAddress | sed 's/"//g' | grep -v null)
+
+# update a dns record
+sed -e "s/IPADDRESS/${IPADDRESS}/" -e "s/COMPONENT/${COMPONENT}/" record.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file://tmp/record.json | jq
